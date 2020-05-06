ce8f2c924653e8e66dc9029fd63d07d000fea63f
Anton Manokhin <a.manokh@gmail.com>
2020-05-06 22:27:32 +0800

    Task 6 init

---
+++ task6_calc/task6.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+#include "task6.h"
+
+using namespace std;
+
+int main() {
+    Stack<double> numbers{};
+    Stack<char> operators{};
+
+    char c;
+    char lastChar = NULL;
+    string numBuff = "";
+    regex r("[\\-\\/+^()*=]");
+    freopen("task6.txt", "r", stdin);
+
+    cout << "RPN: ";
+    while (cin.get(c)) {
+        if (!lastChar && c == '-') {
+            numbers.push(0);
+            cout << 0 << " ";
+            operators.push(c);
+        } else {
+            string s(1, c);
+            if (c == 'e') {                     //получаем вещественную часть
+                char e[5];
+                e[0] = 'e';
+                e[4] = '\0';
+                cin.get(e[1]);
+                cin.get(e[2]);
+                cin.get(e[3]);
+                numBuff += e;
+            } else if (regex_match(s, r)) {             //получаем знак (скобки, +-*/^=)
+                if (numBuff != "") {                    //сбрасываем числовой буфер
+                    numbers.push(stof(numBuff));
+                    cout << numBuff << " ";
+                    numBuff = "";
+                }
+                char lastOperation = operators.get();
+
+                if (c == ')') {                         //прорешиваем все до скобки
+                    while (lastOperation != '(') {
+                        operators.pop();
+                        double a = numbers.pull(), b = numbers.pull();
+                        numbers.push(calculate(a, b, lastOperation));
+                        cout << lastOperation << " ";
+                        lastOperation = operators.get();
+                    }
+                    operators.pop();
+                } else if (c == '*' || c == '/') {        //прорешиваем выскокоприоритетные *, /, ^
+                    while (lastOperation == '*' || lastOperation == '/' || lastOperation == '^') {
+                        operators.pop();
+                        double a = numbers.pull(), b = numbers.pull();
+                        numbers.push(calculate(a, b, lastOperation));
+                        cout << lastOperation << " ";
+                        lastOperation = operators.get();
+                    }
+                    operators.push(c);
+                } else if (c == '+' || c == '-') {        //прорешиваем выскокоприоритетные *, /, ^, +, -
+                    if (lastChar == '(' && c == '-') {
+                        numbers.push(0);
+                        cout << 0 << " ";
+                    } else {
+                        while (lastOperation == '*' || lastOperation == '/' || lastOperation == '^' ||
+                               lastOperation == '+' || lastOperation == '-') {
+                            operators.pop();
+                            double a = numbers.pull(), b = numbers.pull();
+                            numbers.push(calculate(a, b, lastOperation));
+                            cout << lastOperation << " ";
+                            lastOperation = operators.get();
+                        }
+                    }
+                    operators.push(c);
+                } else if (c == '=') {                    //прорешиваем всё
+                    while (lastOperation) {
+                        operators.pop();
+                        double a = numbers.pull(), b = numbers.pull();
+                        numbers.push(calculate(a, b, lastOperation));
+                        cout << lastOperation << " ";
+                        lastOperation = operators.get();
+                    }
+                    operators.push(c);
+                } else {                                  //поступает '(' или '^' - выше по приоритету ничего нет
+                    operators.push(c);
+                }
+            } else {                                    //получаем число
+                numBuff += c;
+            }
+            lastChar = c;
+        }
+    }
+    cout<<endl<<"result: " << numbers.get() << endl;
+}
---
+++ task6_calc/task6.h
@@ -0,0 +1,73 @@
+//
+// Created by amano on 17.03.2020.
+//
+
+#ifndef SPBUTASKS2020_TASK6_H
+#define SPBUTASKS2020_TASK6_H
+#include <bits/stdc++.h>
+
+template <class T>
+struct List {
+    T data;
+    List *next;
+};
+
+template <class T>
+class Stack{
+    List<T> *a;
+public:
+    Stack(){
+        a = new List<T>;
+        a = nullptr;
+        auto *tmp = new List<T>;
+        tmp->data = NULL;
+        tmp->next = a;
+        a = tmp;
+    }
+    void push(T num){
+        auto *tmp = new List<T>;
+        tmp->data = num;
+        tmp->next = a;
+        a = tmp;
+    }
+    T pull(){
+        T data = a->data;
+        a = a->next;
+        return data;
+    }
+    T get(){
+        return a->data;
+    }
+    void pop(){
+        a = a->next;
+    }
+    void print(){
+        List<T> *b = a;
+        while (b){
+            std::cout << b->data << " ";
+            b = b->next;
+        }
+        std::cout << std::endl;
+    }
+
+};
+
+double calculate(double b, double a, char operation) {
+    switch (operation) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            if (b == 0) {
+                std::cerr << "Division by zero! Aborting...";
+                exit(-1);
+            }
+            return a / b;
+        case '^':
+            return pow(a, b);
+    }
+}
+#endif //SPBUTASKS2020_TASK6_H
