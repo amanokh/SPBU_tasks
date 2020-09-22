5c13f0cac652d3e28e67132c0049eadaea16c7cd
Anton Manokhin <a.manokh@gmail.com>
2020-09-22 21:29:04 +0300

    task2

--- sem3/task2_iter/main.cpp
+++ sem3/task2_iter/main.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 int main() {
     freopen("input.txt", "r", stdin);
+
+    int n, q = 0;
+    cin >> n;
+    vector<int> a;
+    vector<int>::iterator iter;
+
+    for (int i = 0; i < n; ++i) {
+        int c;
+        cin >> c;
+        a.push_back(c);
+    }
+
+    for (iter = ++a.begin(); iter < a.end(); ++iter){
+        if (*iter * *prev(iter) < 0) q++;
+    }
+
+    cout << q;
+
     return 0;
 }
